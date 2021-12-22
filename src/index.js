e7a2dc8fc40735875897fad2de4223f50af58f51
David Puerta Martos <dpuerta@usal.es>
2021-12-23 00:03:49 +0100

    create todo and store it in an array

--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import toDo from "./todo.js"
 import css from "./style.css"
 
 let toDoList = []
-const first = new toDo("title1", "description1", "15/10/2022", "high")
-toDoList.push(first)
 
-const createButton = document.querySelector(".submitToDo")
 const titleForm = document.querySelector("#titleForm")
 const descriptionForm = document.querySelector("#descriptionForm")
-createButton.addEventListener("click", ()=>{
-    titleForm.value = "probando"
-    console.log("hola")
+const dateForm = document.querySelector("#dateForm")
+const importantCheck = document.querySelector("#checkPriority")
+const createButton = document.querySelector(".submitToDo")
 
+const restartCreateForm = () => {
+    titleForm.value = ""
+    descriptionForm.value = ""
+    dateForm.value = ""
+    importantCheck.checked = false
+}
+
+createButton.addEventListener("click", ()=>{
+    const newToDo = new toDo(titleForm.value, descriptionForm.value,
+        dateForm.value, importantCheck.checked)
+    toDoList = toDoList.concat(newToDo)
+    console.log(toDoList)
+    restartCreateForm()
 })
