9f8bbb4214b67fe5e5887f3da104d6a9f60d4c4f
David Puerta Martos <dpuerta@usal.es>
2021-12-23 01:22:32 +0100

    projects created

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import toDo from "./todo.js"
+import project from "./projects.js"
 import css from "./style.css"
 
 let toDoList = []
+let projects = []
 
-const titleForm = document.querySelector("#titleForm")
-const descriptionForm = document.querySelector("#descriptionForm")
-const dateForm = document.querySelector("#dateForm")
-const importantCheck = document.querySelector("#checkPriority")
-const createButton = document.querySelector(".submitToDo")
+const createDefaultProject = () => {
+    const defaultProject = new project("default")
+    projects.push(defaultProject)
+}
+
+const createToDo = () => {
+    const newToDo = new toDo(titleForm.value, descriptionForm.value,
+        dateForm.value, importantCheck.checked)
+    toDoList = toDoList.concat(newToDo)
+    const defaultP = projects.filter(project => project.title === "default")[0].list
+    defaultP.push(toDoList)   
+}
 
 const restartCreateForm = () => {
     titleForm.value = ""
@@ -16,10 +25,35 @@ const restartCreateForm = () => {
     importantCheck.checked = false
 }
 
+const refreshToDo = () => {
+    const content = document.querySelector("#content")
+    content.textContent = ""
+    toDoList.map(todo => {
+        const newToDo = document.createElement("div")
+        newToDo.className = "container"
+        const titleDiv = document.createElement("div")
+        titleDiv.textContent = todo.title
+        const dateDiv = document.createElement("div")
+        dateDiv.textContent = todo.dueDate
+        newToDo.append(titleDiv, dateDiv)
+        content.append(newToDo)
+    })
+}
+
+// select all elements from the form and the button to use in the create
+// and restart functions
+const titleForm = document.querySelector("#titleForm")
+const descriptionForm = document.querySelector("#descriptionForm")
+const dateForm = document.querySelector("#dateForm")
+const importantCheck = document.querySelector("#checkPriority")
+const createButton = document.querySelector(".submitToDo")
+
 createButton.addEventListener("click", ()=>{
-    const newToDo = new toDo(titleForm.value, descriptionForm.value,
-        dateForm.value, importantCheck.checked)
-    toDoList = toDoList.concat(newToDo)
-    console.log(toDoList)
+    createToDo()
+    console.log("TOTAL TODOS", toDoList)
+    console.log("PROJECTS", projects)
     restartCreateForm()
+    refreshToDo()
 })
+
+createDefaultProject()
---
+++ src/projects.js
@@ -0,0 +1,6 @@
+function project(title){
+    this.title = title
+    this.list = []
+}
+
+export default project;
